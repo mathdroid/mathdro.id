ca0f7cc4f4e5c001c6c3a64c7052c546ca5c5f10
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2019-03-28 21:40:23 +0700

    add _document

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Mathdroid</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="viewport"
+          />
+          <meta property="og:url" content="https://mathdro.id" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@mathdroid" />
+          <meta name="twitter:creator" content="@mathdroid" />
+          <meta name="twitter:title" content="Hi, my name is Odi!" />
+          <meta name="twitter:description" content="This is my website." />
+          <meta
+            name="twitter:image"
+            content="https://mathdro.id/static/card.jpg"
+          />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 
 const SvgMathdroid = props => (
   <>
@@ -24,21 +23,6 @@ const SvgMathdroid = props => (
 
 const Index = props => (
   <>
-    <Head>
-      <title>Mathdroid</title>
-      <meta
-        name="viewport"
-        content="initial-scale=1.0, width=device-width"
-        key="viewport"
-      />
-      <meta property="og:url" content="https://mathdro.id" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@mathdroid" />
-      <meta name="twitter:creator" content="@mathdroid" />
-      <meta name="twitter:title" content="Hi, my name is Odi!" />
-      <meta name="twitter:description" content="This is my website." />
-      <meta name="twitter:image" content="https://mathdro.id/static/card.jpg" />
-    </Head>
     <SvgMathdroid fill="pink" {...props} />
     <style global jsx>{`
       html,
