12a42d551a9157cb71b23c995bbac2cdddd90976
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-07-28 21:07:48 +0700

    init layout

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,6 +1,8 @@
 import { Heading, Text } from "@chakra-ui/core";
 
-export const H1 = (props) => <Heading {...props} as="h1" color="white" fontWeight="600" />;
+export const H1 = (props) => (
+  <Heading {...props} as="h1" color="white" fontWeight="600" />
+);
 
 export const Headline = (props) => (
   <Heading
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Head from "next/head";
-import { Stack, Link, } from "@chakra-ui/core";
-import { NextSeo } from 'next-seo';
+import { Stack, Link } from "@chakra-ui/core";
+import { NextSeo } from "next-seo";
 
 import { Headline, H1, Paragraph } from "../components/Typography";
 import { SiteButton } from "../components/SiteButton";
 
-const MainCard = (props) => (
+const MainCard = (props) => <Stack spacing="1.5rem" width="100%" {...props} />;
+
+const MainStack = (props) => (
   <Stack
-    spacing="1.5rem"
-    mx="2rem"
-    width="100%"
+    spacing="4rem"
+    margin="0 auto"
+    px="1rem"
+    direction="column"
+    alignItems="center"
     maxWidth="48rem"
     {...props}
   />
-);
-
-const MainStack = (props) => (
-  <Stack spacing="2rem" direction="column" my="8rem" mx="2rem" alignItems="center" {...props} />
 );
 
 const Index = () => {
@@ -33,39 +33,56 @@ const Index = () => {
       products."
       />
       <MainStack>
-        <MainCard
-          py="4rem"
-          px="2rem"
-          borderRadius="8px"
-          background="linear-gradient(#161616, #111 90%)">
-          <Headline>@mathdroid • Odi</Headline>
-          <H1>Muhammad Mustadi</H1>
-          <Paragraph>
-            A full stack software developer, crafting with Typescript, React, and Node.js
-            at the moment. Into hackathons, strategy games, and building useful
-            products.
-          </Paragraph>
-          <Paragraph>Currently available as a contractor/consultant.</Paragraph>
-          <SiteButton onClick={() => {
-            alert('Site is still under construction so here is the email: muhammad.mustadi@gmail.com')
-          }}>
-            Contact mathdroid
-            </SiteButton>        </MainCard>
-        <MainCard>
-          <Headline alignSelf="start">Links</Headline>
-          <Stack spacing="1rem">
+        <Stack spacing="2rem" mt="4rem">
+          <Headline alignSelf={"start"}>@mathdroid</Headline>
+          <MainCard
+            py="2rem"
+            px="1rem"
+            borderRadius="8px"
+            background="linear-gradient(#161616, #111 90%)"
+          >
+            <H1>Muhammad Mustadi</H1>
+            <Paragraph>
+              A full stack software developer, crafting with Typescript, React,
+              and Node.js at the moment. Into hackathons, strategy games, and
+              building useful products.
+            </Paragraph>
+            <Paragraph>
+              Currently available as a contractor/consultant.
+            </Paragraph>
+            <SiteButton
+              onClick={() => {
+                alert(
+                  "Site is still under construction so here is the email: muhammad.mustadi@gmail.com"
+                );
+              }}
+            >
+              Contact mathdroid
+            </SiteButton>
+          </MainCard>
+        </Stack>
 
+        <MainCard>
+          <Headline alignSelf="start">On the web</Headline>
+          <Stack spacing="1rem">
             <SiteButton
               _hover={{
                 bg: "#1da1f2",
               }}
-              as={Link}>Twitter</SiteButton>
-            <Link href="https://github.com/mathdroid"><SiteButton
-              _hover={{
-                color: "black",
-                bg: "white",
-              }}>Github</SiteButton></Link>
-
+              as={Link}
+            >
+              Twitter
+            </SiteButton>
+            <Link href="https://github.com/mathdroid">
+              <SiteButton
+                _hover={{
+                  color: "black",
+                  bg: "white",
+                }}
+              >
+                Github
+              </SiteButton>
+            </Link>
           </Stack>
         </MainCard>
       </MainStack>
