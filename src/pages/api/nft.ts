80e135ba2bca1e9276b83ad8a819ee41def7d25e
Odi <muhammad.mustadi@gmail.com>
2021-10-23 00:19:54 +0700

    add placeholder nft metadata endpoint 2

--- src/pages/api/nft.ts
+++ src/pages/api/nft.ts
@@ -1,8 +1,9 @@
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
+  console.log(req.headers);
   res.json({
-    name: "Placeholder Name",
+    name: "Placeholder Name!",
     description: "Placeholder Descriptopn - mathdroid",
     image: "https://mathdro.id/profile.png",
   });
